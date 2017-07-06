ba2dcf35ed9330e0ca90489677484b4cbf73b040
Garima Batra <gbatra17@gmail.com>
2017-07-05 19:09:21 -0700

    finished queue with functional programming

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,21 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[0];
+    delete storage[0];
+    for(var key in storage){
+      storage[key-1] = storage[key];
+    }
+    delete storage[someInstance.size()-1];
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
