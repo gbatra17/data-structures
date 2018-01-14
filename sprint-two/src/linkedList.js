59993cd7780ebfdd8de3237b75afa510f62f84e8
gbatra17 <gbatra17@gmail.com>
2018-01-14 16:31:19 -0600

    Halway with linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,26 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+
+    //if there is no head
+    if(list.head === null){
+      list.head = newNode;
+    }
+
+    if(list.head){
+      list.tail.next = newNode;
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var oldHead = list.head.value;
+
+    list.head = list.head.next;
+
+    return oldHead;
   };
 
   list.contains = function(target) {
