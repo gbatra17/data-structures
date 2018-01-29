d1d1c04dd4867f619adc232f74e7b664a5fa93a1
gbatra17 <gbatra17@gmail.com>
2018-01-28 23:40:50 -0600

    Working insert method for BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,10 +1,27 @@
 var BinarySearchTree = function(value) {
-  this.left = null;
-  this.right = null;
+  var leaf = Object.create(BinarySearchTree.prototype)
+  leaf.value = value;
+  leaf.left = null;
+  leaf.right = null;
+  return leaf;
 };
 
-BinarySearchTree.prototype.insert = function(){
-
+BinarySearchTree.prototype.insert = function(value){
+  var newBST = new BinarySearchTree(value);
+  if(this.value > value){
+    if(this.left){
+      this.left.insert(value);
+    } else {
+      this.left = newBST;
+    }
+  }
+  if(this.value < value){
+    if(this.right){
+      this.right.insert(value);
+    } else {
+      this.right = newBST;
+    }
+  }
 }
 
 BinarySearchTree.prototype.contains = function(){
